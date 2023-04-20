c035ca3c9ffd75ef4323ed0b7fcc48293c4df1cb
ysmairi <youssef.smairi@inetum.com>
2023-04-20 15:40:38 +0200

    ajout de package service

---
+++ src/main/java/com/demo/api/service/ServiceProductImpl.java
@@ -0,0 +1,14 @@
+package com.demo.api.service;
+
+import com.demo.api.entity.Category;
+import com.demo.api.entity.Product;
+
+public class ServiceProductImpl implements ServiceProduct {
+
+	@Override
+	public Product findProduct(Category cat) {
+		return null;
+	}
+	
+
+}
