712bf33e01ddcc8998337b11c989e39f65524296
ysmairi <youssef.smairi@inetum.com>
2023-04-20 15:24:45 +0200

    ajout de l'entité Product

--- src/main/java/com/demo/api/ApiProductApplication.java
+++ src/main/java/com/demo/api/ApiProductApplication.java
@@ -1,4 +1,4 @@
-package com.test.apiProduct;
+package com.demo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/demo/api/entity/Product.java
@@ -0,0 +1,20 @@
+package com.demo.api.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data @AllArgsConstructor @NoArgsConstructor
+public class Product {
+
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name ;
+	private String marque;
+	private Category category;
+}
